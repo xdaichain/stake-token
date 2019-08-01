3d80f03eb912125638e8fbe899300266d9137583
Max Alekseenko <nnwen0609@gmail.com>
2019-08-01 12:09:45 +0300

    ignore Migrations contract in generating flat files

--- scripts/make_flat.sh
+++ scripts/make_flat.sh
@@ -10,6 +10,9 @@ FULLPATH="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")"
 
 iterate_sources() {
 	for FILE in "$FULLPATH""$1"*.sol; do
+		if [[ $FILE == *"Migrations.sol"* ]]; then
+			break
+		fi
 	    [ -f "$FILE" ] || break
 	    echo $FILE
 	    ./node_modules/.bin/poa-solidity-flattener $FILE $2
