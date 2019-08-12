74db50861ac1a08888a13892166544ea213c586b
Max Alekseenko <nnwen0609@gmail.com>
2019-08-12 19:34:09 +0300

    pass the data to migration script through env and csv-file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,31 +1,30 @@
+const fs = require('fs'); 
+const papaparse = require('papaparse');
+
 const ERC677BridgeToken = artifacts.require('ERC677BridgeToken');
 const Distribution = artifacts.require('Distribution');
-
-const { toWei } = web3.utils;
 
 const TOKEN_NAME = 'DPOS staking token';
 const TOKEN_SYMBOL = 'DPOS';
 const STAKING_EPOCH_DURATION = 604800; // in seconds
 
-module.exports = async (deployer, network, accounts) => {
-  const REWARD_FOR_STAKING_ADDRESS = accounts[1];
-  const ECOSYSTEM_FUND_ADDRESS = accounts[2];
-  const PUBLIC_OFFERING_ADDRESS = accounts[3];
-  const FOUNDATION_REWARD_ADDRESS = accounts[4];
-  const EXCHANGE_RELATED_ACTIVITIES_ADDRESS = accounts[5];
-  const privateOfferingParticipants = [accounts[6], accounts[7]];
-  const privateOfferingParticipantsStakes = [toWei('3000000'), toWei('5500000')];
+module.exports = async deployer => {
+  const csvData = fs.readFileSync(process.env.PRIVATE_OFFERING_DATA, { encoding: 'utf8' });
+  const privateOfferingData = papaparse.parse(csvData, { delimiter: ',', header: true, skipEmptyLines: true }).data;
+
+  const privateOfferingParticipants = privateOfferingData.map(item => item.participant);
+  const privateOfferingParticipantsStakes = privateOfferingData.map(item => item.stake);
 
   await deployer;
 
   const distribution = await deployer.deploy(
     Distribution,
     STAKING_EPOCH_DURATION,
-    REWARD_FOR_STAKING_ADDRESS,
-    ECOSYSTEM_FUND_ADDRESS,
-    PUBLIC_OFFERING_ADDRESS,
-    FOUNDATION_REWARD_ADDRESS,
-    EXCHANGE_RELATED_ACTIVITIES_ADDRESS,
+    process.env.REWARD_FOR_STAKING_ADDRESS,
+    process.env.ECOSYSTEM_FUND_ADDRESS,
+    process.env.PUBLIC_OFFERING_ADDRESS,
+    process.env.FOUNDATION_REWARD_ADDRESS,
+    process.env.EXCHANGE_RELATED_ACTIVITIES_ADDRESS,
     privateOfferingParticipants,
     privateOfferingParticipantsStakes
   );
@@ -37,5 +36,9 @@ module.exports = async (deployer, network, accounts) => {
     distribution.address
   );
 
-  // await distribution.initialize(token.address);
+  await distribution.initialize(token.address);
 };
+
+
+// example
+// REWARD_FOR_STAKING_ADDRESS=0xd35114b4cef03065b0fa585d1c2e15e8fb589507 ECOSYSTEM_FUND_ADDRESS=0xb28a3211ca4f9bf8058a4199acd95c999c4cdf3b PUBLIC_OFFERING_ADDRESS=0x975fe74ec9cc82afdcd8393ce96abe039c6dba84 FOUNDATION_REWARD_ADDRESS=0xb68d0a5c0566c39e8c2f8e15d8494032fd420da1 EXCHANGE_RELATED_ACTIVITIES_ADDRESS=0x7f29ce8e46d01118888b1692f626d990318018ea PRIVATE_OFFERING_DATA=./example.csv ./node_modules/.bin/truffle migrate --reset
