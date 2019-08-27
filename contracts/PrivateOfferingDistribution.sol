107f9d1a74d1d6c5048092a3be0b41a0be60332e
Max Alekseenko <nnwen0609@gmail.com>
2019-08-27 19:21:22 +0300

    remove restriction

--- contracts/PrivateOfferingDistribution.sol
+++ contracts/PrivateOfferingDistribution.sol
@@ -63,7 +63,7 @@ contract PrivateOfferingDistribution is Ownable, IPrivateOfferingDistribution {
         require(_participants.length == _stakes.length, "different arrays sizes");
         uint256 sumOfStakes = 0;
         for (uint256 i = 0; i < _participants.length; i++) {
-            require(_participants[i] != address(0) && _participants[i] != owner(), "invalid address");
+            require(_participants[i] != address(0), "invalid address");
             require(_stakes[i] > 0, "the participant stake must be more than 0");
             sumOfStakes = sumOfStakes.add(_stakes[i]);
         }
