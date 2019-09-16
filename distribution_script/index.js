1b6c126af65aeea9833a0d15d61f5c17bed8954d
Max Alekseenko <nnwen0609@gmail.com>
2019-09-16 14:52:37 +0300

    make server port configurable

--- distribution_script/index.js
+++ distribution_script/index.js
@@ -7,7 +7,7 @@ require('./worker');
 const app = express();
 
 app.use('', router);
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
     console.log('App is running at http://localhost:%d', app.get('port'));
 });
