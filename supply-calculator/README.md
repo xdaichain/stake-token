62266fbc7498a24240be0142e4be10b15957be04
Vadim <varasev@gmail.com>
2020-09-03 15:39:35 +0300

    Add /health request handling to supply-calculator

--- supply-calculator/README.md
+++ supply-calculator/README.md
@@ -1,7 +1,13 @@
-# STAKE token circulating supply calculator
+# STAKE token supply calculator
 
-This is a simple script for calculating the circulating supply of STAKE tokens on Ethereum Mainnet.
+This is a simple script for calculating the circulating and total supply of STAKE tokens on Ethereum Mainnet.
 
 To run the script on a live server with `Docker Compose` use [these instructions](https://github.com/xdaichain/stake-token/tree/docker-compose/supply-calculator#readme).
 
-Currently, the script is deployed and lives at http://64.225.104.36:3000/
+Currently, the script is deployed and lives at `supply.xdaichain.com`.
+
+To get circulating supply, go to https://supply.xdaichain.com/
+
+To get total supply, go to https://supply.xdaichain.com/total
+
+To get health status in JSON, use https://supply.xdaichain.com/health
--- supply-calculator/index.js
+++ supply-calculator/index.js
@@ -4,7 +4,9 @@ const Web3 = require('web3');
 const web3 = new Web3(process.env.RPC_URL || 'https://mainnet.infura.io/v3/1125fe73d87c4e5396678f4e3089b3dd');
 const BN = web3.utils.BN;
 
-// Token contract instance
+const refreshInterval = parseInt(process.env.REFRESH_INTERVAL || 10); // in seconds
+
+// Token contract instance on Ethereum Mainnet
 const tokenContract = new web3.eth.Contract(
   [{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}],
   '0x0Ae055097C6d159879521C384F1D2123D1f195e6'
@@ -18,7 +20,13 @@ if (!burnAddresses.includes(zeroAddress)) {
   burnAddresses.push(zeroAddress);
 }
 
-// Output circulating supply on demand
+// Health API variables
+let status = 'success'; // can be 'success' or 'error'
+let lastSuccessDatetime = '';
+let lastErrorDatetime = '';
+let lastError = '';
+
+// Handle HTTP requests
 let circulatingSupply = '0';
 let totalSupply = '0';
 const server = http.createServer(async (req, res) => {
@@ -28,6 +36,24 @@ const server = http.createServer(async (req, res) => {
   } else if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(circulatingSupply);
+  } else if (req.url === '/health') {
+    res.writeHead(200, { 'Content-Type': 'text/json' });
+
+    const lastSuccessTimestamp = lastSuccessDatetime ? Date.parse(lastSuccessDatetime) / 1000 : 0;
+    const lastErrorTimestamp = lastErrorDatetime ? Date.parse(lastErrorDatetime) / 1000 : 0;
+    const secondsSinceLastSuccess = Math.floor(Date.now() / 1000 - lastSuccessTimestamp);
+    const secondsSinceLastError = Math.floor(Date.now() / 1000 - lastErrorTimestamp);
+
+    res.end(JSON.stringify({
+      status,
+      currentDatetime: currentDatetime(),
+      lastSuccessDatetime,
+      secondsSinceLastSuccess,
+      lastErrorDatetime,
+      secondsSinceLastError,
+      lastError,
+      supplyRefreshInterval: refreshInterval
+    }, null, 2));
   } else {
     res.writeHead(404);
     res.end();
@@ -38,32 +64,52 @@ readSupply();
 
 // Reads and calculates supply
 async function readSupply() {
-  totalSupply = new BN(await tokenContract.methods.totalSupply().call());
-  const distributionBalance = new BN(await tokenContract.methods.balanceOf('0x9BC4a93883C522D3C79c81c2999Aab52E2268d03').call());
-  const privateOfferingBalance = new BN(await tokenContract.methods.balanceOf('0x3cFE51b61E25750ab1426b0072e5D0cc5C30aAfA').call());
-  const advisorsRewardBalance = new BN(await tokenContract.methods.balanceOf('0x0218B706898d234b85d2494DF21eB0677EaEa918').call());
-  
-  let zeroBalance = new BN(0);
-  for (let i = 0; i < burnAddresses.length; i++) {
-    zeroBalance = zeroBalance.add(new BN(await tokenContract.methods.balanceOf(burnAddresses[i]).call()));
+  try {
+    const totalSupplyBN = new BN(await tokenContract.methods.totalSupply().call());
+
+    if (totalSupplyBN.isZero()) {
+      throw Error('Error: totalSupply is zero');
+    } else {
+      totalSupply = web3.utils.fromWei(totalSupplyBN);
+    }
+
+    const distributionBalance = new BN(await tokenContract.methods.balanceOf('0x9BC4a93883C522D3C79c81c2999Aab52E2268d03').call());
+    const privateOfferingBalance = new BN(await tokenContract.methods.balanceOf('0x3cFE51b61E25750ab1426b0072e5D0cc5C30aAfA').call());
+    const advisorsRewardBalance = new BN(await tokenContract.methods.balanceOf('0x0218B706898d234b85d2494DF21eB0677EaEa918').call());
+    
+    let zeroBalance = new BN(0);
+    for (let i = 0; i < burnAddresses.length; i++) {
+      zeroBalance = zeroBalance.add(new BN(await tokenContract.methods.balanceOf(burnAddresses[i]).call()));
+    }
+
+    circulatingSupply = web3.utils.fromWei(
+      totalSupplyBN
+        .sub(distributionBalance)
+        .sub(privateOfferingBalance)
+        .sub(advisorsRewardBalance)
+        .sub(zeroBalance)
+    );
+
+    log(`${circulatingSupply}, ${totalSupply}`);
+
+    status = 'success';
+    lastSuccessDatetime = currentDatetime();
+  } catch (e) {
+    status = 'error';
+    lastError = e.message;
+    lastErrorDatetime = currentDatetime();
+    log(e.message);
   }
 
-  circulatingSupply = web3.utils.fromWei(
-    totalSupply
-      .sub(distributionBalance)
-      .sub(privateOfferingBalance)
-      .sub(advisorsRewardBalance)
-      .sub(zeroBalance)
-  );
-  totalSupply = web3.utils.fromWei(totalSupply);
-
-  log(`${circulatingSupply}, ${totalSupply}`);
-
-  setTimeout(readSupply, (process.env.REFRESH_INTERVAL || 10)*1000); // update every N seconds
+  setTimeout(readSupply, refreshInterval * 1000); // update every refreshInterval seconds
 }
 
 // Prints log message with the current time
 function log(message) {
+  console.log(`${currentDatetime()} ${message}`);
+}
+
+function currentDatetime() {
   const now = new Date;
   const year = now.getUTCFullYear();
   const month = (now.getUTCMonth() - 0 + 1).toString().padStart(2, '0');
@@ -71,6 +117,5 @@ function log(message) {
   const hours = (now.getUTCHours() - 0).toString().padStart(2, '0');
   const minutes = (now.getUTCMinutes() - 0).toString().padStart(2, '0');
   const seconds = (now.getUTCSeconds() - 0).toString().padStart(2, '0');
-  const time = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} UTC`;
-  console.log(`${time} ${message}`);
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} UTC`;
 }
